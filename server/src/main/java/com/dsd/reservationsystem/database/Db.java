6ff3bb2361a93dac72d437f6796bb46bcbd77077
jesus doza <jesus.doza@gmail.com>
2024-03-26 20:35:10 -0500

    removed unused import

--- server/src/main/java/com/dsd/reservationsystem/database/Db.java
+++ server/src/main/java/com/dsd/reservationsystem/database/Db.java
@@ -10,7 +10,6 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
 
-import java.awt.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
