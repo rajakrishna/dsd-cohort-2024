7ca9357ef1524e2dd0056bc7a35169398621d43c
jesus doza <jesus.doza@gmail.com>
2024-03-26 19:59:08 -0500

    add timeslots controller

---
+++ server/src/main/java/com/dsd/reservationsystem/controller/TimeSlotsController.java
@@ -0,0 +1,32 @@
+package com.dsd.reservationsystem.controller;
+
+
+import com.dsd.reservationsystem.database.Db;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/timeslots")
+public class TimeSlotsController {
+
+    private Db database;
+
+
+
+    public TimeSlotsController(Db database) {
+        this.database = database;
+    }
+
+
+//    @GetMapping("")
+//    public String getTimeSlots(){
+//        return "timeslots";
+//    }
+    @GetMapping("")
+    public List getTimeSlots(){
+        return this.database.getTimeSlots();
+    }
+}
