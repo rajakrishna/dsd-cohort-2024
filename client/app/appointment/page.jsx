44d5effc68b931856df01af792cb888cdfb710ee
George Liu <georgeliu8110@gmail.com>
2024-03-28 17:46:13 -0500

    adjust style and add fetches

--- client/app/appointment/page.jsx
+++ client/app/appointment/page.jsx
@@ -1,15 +1,17 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { mockServicesData } from "../mockData/mockGetServicesApi";
 import { mockTimeSlotsData } from "../mockData/mockGetTimeSlotsApi";
 import { timeSlots, statesList } from "@/constants";
-import { router } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function AppointmentPage() {
 
+  const [serviceList, setServiceList] = useState(mockServicesData)
+  const [timeSlotsList, setTimeSlotsList] = useState(mockTimeSlotsData)
   const [service, setService] = useState('')
   const [startDate, setStartDate] = useState(new Date().toLocaleDateString())
   const [appointment, setAppointment] = useState('')
@@ -22,6 +24,7 @@ export default function AppointmentPage() {
   const [state, setState] = useState('')
   const [city, setCity] = useState('')
   const [zip, setZip] = useState('')
+  const router = useRouter()
 
   const serviceInputHandler = (e) => {
     setService(e.target.value)
@@ -68,9 +71,14 @@ export default function AppointmentPage() {
   }
 
   const bookAppointment = () => {
-console.log(startDate)
+
+    if (!service || !startDate || !appointment || !firstName || !lastName || !email || !phone || !street || !state || !city || !zip) {
+      alert('Please fill out all fields')
+      return
+    }
+
     const timeSlotKey = Object.keys(timeSlots).find(timeSlotKey => timeSlots[timeSlotKey] === appointment);
-    const selectedService = mockServicesData.find(item => item.name === service);
+    const selectedService = serviceList.find(item => item.name === service);
     const formattedDate = startDate.split('/')
     let [month, day, year] = formattedDate
     if (month.length === 1) {
@@ -91,9 +99,9 @@ console.log(startDate)
         "serviceId": selectedService.id
       }
     }
-    console.log(data)
+
     fetch(
-      '/api/appointment',{
+      '/saveAppointmentApi', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -101,40 +109,89 @@ console.log(startDate)
       body: JSON.stringify(data)
     }
     )
-    .then(
-      router.push('/appointmentconfirmed')
-    )
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === 'success') {
+        router.push('/appointmentconfirmed')
+      }
+    })
     .catch(err => console.log(err))
   }
 
+  //will enable below once the backend is ready!
+  //====================================================================================================
+  // const getServiceList = () => {
+  //   fetch(
+  //     '/getServicesApi', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }
+  //   )
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     if (data.status === 'success') {
+  //       setServiceList(data.services)
+  //     }
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+
+  // const getTimeSlotsList = () => {
+  //   fetch(
+  //     '/getTimeSlotsApi/${startDate}', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }
+  //   )
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     if (data.status === 'success') {
+  //       setTimeSlotsList(data.timeSlots)
+  //     }
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+
+  // useEffect(() => {
+  //   getServiceList()
+  // }, [])
+
+  // useEffect(() => {
+  //   getTimeSlotsList()
+  // }, [startDate])
+  //====================================================================================================
   return (
   <div className="flex flex-row min-h-screen w-full">
     <form className='flex-1 pr-2 grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-1 p-40'>
       <div className="col-span-1 md:col-span-2 lg:col-span-2">
         <select className="select select-bordered select-lg w-full" onChange={serviceInputHandler}>
           <option disabled selected>Services</option>
-          {mockServicesData.map(service => <option key={service.id}>{service.name}</option>)}
+          {serviceList.map(service => <option key={service.id}>{service.name}</option>)}
         </select>
       </div>
 
       <div className="dropdown col-span-1">
         <div tabIndex={0} role="button" className="btn btn-lg w-full col-span-1 ">Service Date - {startDate}</div>
-        <div tabIndex={0} className="dropdown-content z-[1] card card-compact w-64 p-2 shadow bg-white text-primary-content">
+        <div tabIndex={0} className="dropdown-content z-[1] card card-compact w-64 p-2 shadow bg-blue-500 text-primary-content">
           <div className="card-body">
-            <h3 className="card-title">Please select the date</h3>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date.toLocaleDateString())} />
+            <h3 className="card-title text-white">Please select the date</h3>
+            <DatePicker className='text-white' selected={startDate} onChange={(date) => setStartDate(date.toLocaleDateString())} />
           </div>
         </div>
       </div>
       <div className="col-span-1">
         <select className="select select-bordered select-lg w-full" onChange={appointmentInputHandler}>
           <option disabled selected>Appointment Time Slots</option>
-          {mockTimeSlotsData.TS79 && <option>{timeSlots.TS79}</option>}
-          {mockTimeSlotsData.TS911 && <option>{timeSlots.TS911}</option>}
-          {mockTimeSlotsData.TS111 && <option>{timeSlots.TS111}</option>}
-          {mockTimeSlotsData.TS13 && <option>{timeSlots.TS13}</option>}
-          {mockTimeSlotsData.TS35 && <option>{timeSlots.TS35}</option>}
-          {mockTimeSlotsData.TS57 && <option>{timeSlots.TS57}</option>}
+          {timeSlotsList.TS79 && <option>{timeSlots.TS79}</option>}
+          {timeSlotsList.TS911 && <option>{timeSlots.TS911}</option>}
+          {timeSlotsList.TS111 && <option>{timeSlots.TS111}</option>}
+          {timeSlotsList.TS13 && <option>{timeSlots.TS13}</option>}
+          {timeSlotsList.TS35 && <option>{timeSlots.TS35}</option>}
+          {timeSlotsList.TS57 && <option>{timeSlots.TS57}</option>}
         </select>
       </div>
 
@@ -148,11 +205,11 @@ console.log(startDate)
         <option disabled selected>State</option>
         {statesList.map(state => <option key={state.abbreviation}>{state.name}</option>)}
       </select>
-       <input type="text" placeholder='City' className="input input-bordered input-lg w-full" />
-       <input type="text" placeholder='Zipcode' className="input input-bordered input-lg w-full" />
+       <input type="text" placeholder='City' className="input input-bordered input-lg w-full" onChange={cityInputHandler}/>
+       <input type="text" placeholder='Zipcode' className="input input-bordered input-lg w-full" onChange={zipInputHandler}/>
     </form>
     <div className="flex-1 flex flex-col justify-center items-center p-5">
-      <div className="w-full max-w-lg border-4 border-gray-300 shadow-md py-20 h-auto px-8">
+      <div className="w-full max-w-lg border-4 rounded-3xl border-gray-300 shadow-md py-20 h-auto px-8">
         <h1 className='pb-6 font-bold text-3xl'>Appointment Summary:</h1>
         <p className='py-5 text-xl'>Name: {firstName + ' ' + lastName}</p>
         <p className='py-5 text-xl'>Phone number: {phone}</p>
@@ -161,7 +218,7 @@ console.log(startDate)
         <p className='py-5 text-xl'>Selected time: {appointment}</p>
       </div>
       <div>
-        <button className="btn btn-primary mt-5" onClick={bookAppointment}>Book Appointment</button>
+        <button className="btn btn-primary mt-8 " onClick={bookAppointment}>Book Appointment</button>
       </div>
     </div>
   </div>
