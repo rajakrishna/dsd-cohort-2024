7f007b9b85f5ddd15a5f4737f5c1f6302d45f480
Pkasu06 <115344836+Pkasu06@users.noreply.github.com>
2024-03-30 20:40:57 +0000

    button styling based on current page

--- client/app/admin/layout.jsx
+++ client/app/admin/layout.jsx
@@ -1,6 +1,11 @@
+'use client'
 import Link from "next/link";
 import { sideNavItems } from "@/constants";
+import { usePathname } from 'next/navigation'
+
 export default function Layout({ children }) {
+  const pathname = usePathname();
+
   return (
     <div className="drawer lg:drawer-open bg-white">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -12,9 +17,9 @@ export default function Layout({ children }) {
         <label aria-label="close sidebar" className="drawer-overlay"></label> 
         <ul className="menu p-4 w-80 min-h-full text-base-content">
           {sideNavItems.map((item) => (
-            <li key={item.label} className="mb-4 border-2 border-black rounded-lg hover:bg-gray-200 text-center">
+            <li key={item.label} className={`mb-4 border-2 border-black rounded-lg text-center ${pathname === item.href ? "bg-blue-500" : ""}` }>
               <Link href={item.href}>
-                  <button className="text-black p-2 font-bold block">{item.label}</button>
+                  <button className={`p-2 font-bold block ${pathname === item.href ? "text-white" : "text-black"}`}>{item.label}</button>
               </Link>
             </li>
           ))}
