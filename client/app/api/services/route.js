f6b5aeee9e74ab9029c44aae76c0c8073da5371f
jesusdoza <jesus.doza@gmail.com>
2024-04-02 18:43:42 -0500

    add services get request api endpoint and java integration

--- client/app/api/services/route.js
+++ client/app/api/services/route.js
@@ -1,5 +1,29 @@
+const API_URL = process.env.API_URL;
+
 export async function GET(request) {
-  return new Response("Hello, Next.js!", {
-    status: 200,
-  });
+  try {
+    const data = await getServicesList();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+    });
+  } catch (error) {}
 }
+
+async function getServicesList() {
+  const response = await fetch(`${API_URL}/services`);
+  const data = await response.json();
+  console.log("data", data);
+  return data;
+}
+
+// Response.json({
+//   Mes: "Hello, Next.js!",
+//   env: process.env.API_URL,
+// });
+
+// return new Response(
+//   { Mes: "Hello, Next.js!", env: process.env.API_URL },
+//   {
+//     status: 200,
+//   }
+// );
