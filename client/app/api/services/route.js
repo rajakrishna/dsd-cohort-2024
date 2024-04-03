7b4056ae9974ae821dab8c18a1b2bb1dfccd06ea
jesusdoza <jesus.doza@gmail.com>
2024-04-02 19:01:46 -0500

    add error handling to get request

--- client/app/api/services/route.js
+++ client/app/api/services/route.js
@@ -1,18 +1,27 @@
 const API_URL = process.env.API_URL;
+const LOC = "/api/services";
 
 export async function GET(request) {
   try {
     const data = await getServicesList();
-    return new Response(JSON.stringify(data), {
+    const dataJson = JSON.stringify(data);
+    return new Response(dataJson, {
       status: 200,
     });
-  } catch (error) {}
+  } catch (error) {
+    const message = JSON.stringify({
+      error: "failed to get services list",
+      LOC,
+    });
+    return new Response(message, {
+      status: 500,
+    });
+  }
 }
 
 async function getServicesList() {
   const response = await fetch(`${API_URL}/services`);
   const data = await response.json();
-  console.log("data", data);
   return data;
 }
 
