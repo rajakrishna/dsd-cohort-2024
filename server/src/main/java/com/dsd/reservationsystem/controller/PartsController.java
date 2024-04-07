dd4bb30b48e2af42977a7bd0845d8d578a70c4c2
jesus doza <jesus.doza@gmail.com>
2024-04-06 19:51:22 -0500

    Revert "changes made to JAVA Parts controller"
    This reverts commit 31cca148d2d0167f982ff6b6389794600f1f91b2.

--- server/src/main/java/com/dsd/reservationsystem/controller/PartsController.java
+++ server/src/main/java/com/dsd/reservationsystem/controller/PartsController.java
@@ -1,10 +1,10 @@
 package com.dsd.reservationsystem.controller;
 
+
 import com.dsd.reservationsystem.models.Part;
 import com.dsd.reservationsystem.service.PartsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,14 +15,14 @@ public class PartsController {
 
   private PartsService partsService;
 
+
   public PartsController(PartsService partsService) {
     this.partsService = partsService;
   }
 
+
   @GetMapping("")
-  public List<Part> getPartsInventory(@RequestParam("lowInventory") Boolean lowInventory) {
-    // todo WORKING HERE getting low inventory param
-    System.out.println("inventory low param " + lowInventory);
+  public List<Part> getPartsInventory() {
     return this.partsService.getAll();
   }
 }
