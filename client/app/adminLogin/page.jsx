b0dfc5b00578a7509691a9d94331fe278f14ec70
George Liu <georgeliu8110@gmail.com>
2024-04-07 13:17:54 -0500

    Co-authored-by: Jesus Mendoza <jesusdoza@users.noreply.github.com>

---
+++ client/app/adminLogin/page.jsx
@@ -0,0 +1,31 @@
+export default function AdminLogin() {
+  return (
+  <div className='flex min-h-screen'>
+  <form className='w-1/2 flex flex-col justify-center p-5'>
+    <h1 className="text-center mb-4">Admin Login</h1>
+    <label className="input input-bordered flex items-center gap-2 mb-4">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+      <input type="text" className="grow" placeholder="Email" />
+    </label>
+    <label className="input input-bordered flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+      <input type="password" className="grow" value="password" />
+    </label>
+    <button type='submit' className="btn btn-primary my-10 mx-80">Login</button>
+  </form>
+  <div className="w-px bg-gray-200 my-auto h-80"></div>
+  <form className='w-1/2 flex flex-col justify-center p-5 border-gray-200'>
+    <h1 className="text-center mb-4">Admin SignUp</h1>
+    <label className="input input-bordered flex items-center gap-2 mb-4">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+      <input type="text" className="grow" placeholder="Email" />
+    </label>
+    <label className="input input-bordered flex items-center gap-2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+      <input type="password" className="grow" value="password" />
+    </label>
+    <button type='submit' className="btn btn-primary my-10 mx-80">Sign Up</button>
+  </form>
+  </div>
+  )
+}
