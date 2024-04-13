023d829e7a5917135763ae92312d971c9834742f
George Liu <georgeliu8110@gmail.com>
2024-04-13 14:37:46 -0500

    add error message for sign up

--- client/app/signup/page.jsx
+++ client/app/signup/page.jsx
@@ -14,6 +14,16 @@ export default function LogUpPage() {
   const [user, loading, error] = useAuthState(auth);
   const router = useRouter()
 
+  useEffect(() => {
+    if (signUpError) {
+      if (signUpError.message.includes('email-already-in-use')) {
+        setSignUpAlert('Email already in use');
+      } else {
+        setSignUpAlert('');
+      }
+    }
+  }, [signUpError]);
+
   const signUpFormSubmitHandler = async (e) => {
     e.preventDefault()
     try {
@@ -31,7 +41,7 @@ export default function LogUpPage() {
   return (
   <div className=" flex items-center justify-center h-screen">
     <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h1 className="text-xl font-bold mb-4">Sign In</h1>
+        <h1 className="text-xl font-bold mb-4 text-blue-500 text-center">Sign Up</h1>
         <form onSubmit={signUpFormSubmitHandler}>
             <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
@@ -39,8 +49,13 @@ export default function LogUpPage() {
             </div>
             <div className="mb-6">
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                <input type="password" id="password" className="mt-1 p-2 w-full border rounded-md" onChange={(e) => setSignupPassword(e.target.value)} required />
+                <input type="password" id="password" className="mt-1 p-2 w-full border rounded-md" onChange={(e) => setSignupPassword(e.target.value)} minLength='6' required />
             </div>
+            <div className='min-h-[2em]'>
+            {!signUpError ?
+            <p className={`text-blue-500 text-xs py-2 ${signUpLoading ? 'visible' : 'invisible'}`}>please wait while signing you up ...</p> :
+            <p className={`text-red-500 text-xs py-2 ${signUpError ? 'visible' : 'invisible'}`}>{signUpError ? signUpAlert : ' '}</p>}
+          </div>
             <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">Sign Up</button>
         </form>
     </div>
