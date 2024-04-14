c2e082a46172476bdaa41b5d63387873d4a60873
jesus doza <jesus.doza@gmail.com>
2024-04-14 09:26:31 -0500

    add Appointment post request class

--- server/src/main/java/com/dsd/reservationsystem/controller/AppointmentController.java
+++ server/src/main/java/com/dsd/reservationsystem/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package com.dsd.reservationsystem.controller;
 
 import com.dsd.reservationsystem.models.Appointment;
+import com.dsd.reservationsystem.models.AppointmentPostRequest;
 import com.dsd.reservationsystem.service.AppointmentService;
 import com.dsd.reservationsystem.service.TimeSlotsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,40 +16,42 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/api/appointment")
 public class AppointmentController {
 
-  @Autowired
-  private AppointmentService appointmentService;
+    @Autowired
+    private AppointmentService appointmentService;
 
-  @Autowired
-  private TimeSlotsService timeSlotsService;
+    @Autowired
+    private TimeSlotsService timeSlotsService;
 
-  @PostMapping("/save")
-  public ResponseEntity saveAppointment(@RequestBody Appointment appointmentRequest) {
+    @PostMapping("/save")
+    public ResponseEntity saveAppointment(@RequestBody AppointmentPostRequest appointmentRequest) {
 
-    System.out.println("Appointment request is :");
-    System.out.println(appointmentRequest);
+        System.out.println("Appointment request is :");
+        System.out.println(appointmentRequest);
 
-    // Save the appointment
-    try {
-      Appointment savedAppointment = appointmentService.saveAppointment(appointmentRequest);
-//    Appointment createdAppointment = new Appointment();
-        
-      return new ResponseEntity<Appointment>(savedAppointment, HttpStatus.OK);
-    } catch (Exception e) {
 
-      //todo test error sent back
-      return new ResponseEntity("Error :" + e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        // Save the appointment
+//        try {
+//            Appointment savedAppointment = appointmentService.saveAppointment(appointmentRequest);
+//
+//
+//            return new ResponseEntity<Appointment>(savedAppointment, HttpStatus.OK);
+//        } catch (Exception e) {
+//
+//            //todo test error sent back
+//            return new ResponseEntity("Error :" + e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+        return new ResponseEntity("savedAppointment", HttpStatus.OK);
     }
-  }
 
-  private boolean isTimeSlotAvailable(String day, String timeSlot) {
-    // Get the time slots for the selected day
-    return timeSlotsService.isTimeSlotAvailable(day, timeSlot);
+    private boolean isTimeSlotAvailable(String day, String timeSlot) {
+        // Get the time slots for the selected day
+        return timeSlotsService.isTimeSlotAvailable(day, timeSlot);
 
-  }
+    }
 
-  @GetMapping("/all")
-  public ResponseEntity<List<Appointment>> getAppointments(@RequestParam String date) throws ExecutionException, InterruptedException {
-    List<Appointment> appointments = appointmentService.getAppointmentsForDay(date);
-    return ResponseEntity.ok(appointments);
-  }
+    @GetMapping("/all")
+    public ResponseEntity<List<Appointment>> getAppointments(@RequestParam String date) throws ExecutionException, InterruptedException {
+        List<Appointment> appointments = appointmentService.getAppointmentsForDay(date);
+        return ResponseEntity.ok(appointments);
+    }
 }
