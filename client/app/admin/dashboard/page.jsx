97a91b4bde5a1f34d09e903590f2fcf62ee182a7
Raja Krishna <rajakrishna214@gmail.com>
2024-04-19 21:00:08 -0500

    bugfix - appointment page

--- client/app/admin/dashboard/page.jsx
+++ client/app/admin/dashboard/page.jsx
@@ -106,106 +106,96 @@ export default function AdminPage() {
 	let lowInventoryList = findLowInventoryParts(getPartsData);
 
 	return (
-		<div className='flex-1 flex flex-col justify-center items-center p-10'>
-			<div className='w-full max-w-full border-2 rounded-3xl border-black shadow-md py-20 h-auto px-20 overflow-auto mb-10'>
-				<h1 className='text-black text-center pb-10 font-bold'>Today's Appointments</h1>
-				<table className=' text-black w-full min-w-full divide-y divide-gray-200'>
-					<thead className='text-black w-full max-w-full border-2 border-black rounded-3xl bg-gray-50'>
-						<tr className='mb-7'>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'></th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Name</th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Time</th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-								Service
-							</th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-								Parts Needed
-							</th>
-						</tr>
-					</thead>
-					<tbody className='bg-white divide-y divide-gray-200 mt-4 overflow-scroll max-h-max'>
-						{displayedRows.map((app, index) => {
-							return (
-								<tr className='hover' key={uuidv4()}>
-									<th className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{indexOfFirstRow + index + 1}
-									</th>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{app.name}
-									</td>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{timeSlots[app.time]}
-									</td>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{serviceName(app.serviceId)}
-									</td>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{partNameCollection(app.serviceId)}
-									</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-				{totalPages > 1 && (
-					<div className='flex text-blue-500 items-center justify-between pt-5'>
-						{!(currentPage === 1) ? (
-							<button
-								onClick={prevPage}
-								disabled={currentPage === 1}
-								className='flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white mr-20'
-							>
-								Previous
-							</button>
-						) : (
-							<div className='flex-1'></div>
-						)}
-						{!(currentPage === totalPages) ? (
-							<button
-								onClick={nextPage}
-								disabled={currentPage === totalPages}
-								className='flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-white bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ml-20'
-							>
-								Next
-							</button>
-						) : (
-							<div className='flex-1'></div>
-						)}
-					</div>
-				)}
-			</div>
+		<>
+			<h1 className='text-black text-center pb-4 font-bold'>Today's Appointments</h1>
+			<table className=' text-black w-full min-w-full divide-y divide-gray-200'>
+				<thead className='text-black w-full max-w-full border-2 border-black rounded-3xl bg-gray-50'>
+					<tr className='mb-7'>
+						<th></th>
+						<th>Name</th>
+						<th>Time</th>
+						<th>Service</th>
+						<th>Parts Needed</th>
+					</tr>
+				</thead>
+				<tbody className='bg-white divide-y divide-gray-200 mt-4 overflow-scroll max-h-max'>
+					{displayedRows.map((app, index) => {
+						return (
+							<tr className='hover' key={uuidv4()}>
+								<th className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{indexOfFirstRow + index + 1}
+								</th>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{app.name}
+								</td>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{timeSlots[app.time]}
+								</td>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{serviceName(app.serviceId)}
+								</td>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{partNameCollection(app.serviceId)}
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+			{totalPages > 1 && (
+				<div className='flex text-blue-500 items-center justify-between pt-5'>
+					{!(currentPage === 1) ? (
+						<button
+							onClick={prevPage}
+							disabled={currentPage === 1}
+							className='flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white mr-20'
+						>
+							Previous
+						</button>
+					) : (
+						<div className='flex-1'></div>
+					)}
+					{!(currentPage === totalPages) ? (
+						<button
+							onClick={nextPage}
+							disabled={currentPage === totalPages}
+							className='flex items-center justify-center px-4 h-10 ms-3 text-base font-medium text-white bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ml-20'
+						>
+							Next
+						</button>
+					) : (
+						<div className='flex-1'></div>
+					)}
+				</div>
+			)}
 
-			<div className='w-full max-w-full border-2 rounded-3xl border-black shadow-md py-20 h-auto px-20 overflow-y-auto max-h-[500px]'>
-				<h1 className='text-black text-center pb-10 font-bold'>Low Inventory Summary</h1>
-				<table className=' text-black w-full min-w-full divide-y divide-gray-200'>
-					<thead className='text-black w-full max-w-full border-2 border-black rounded-3xl bg-gray-50'>
-						<tr className='mb-7'>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'></th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Name</th>
-							<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-								Quantity
-							</th>
-						</tr>
-					</thead>
-					<tbody className='bg-white divide-y divide-gray-200 mt-4 overflow-scroll max-h-max'>
-						{lowInventoryList.map((item, index) => {
-							return (
-								<tr className='hover' key={uuidv4()}>
-									<th className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{index + 1}
-									</th>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{item.name}
-									</td>
-									<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
-										{item.quantity}
-									</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-			</div>
-		</div>
+			<h1 className='text-black text-center pb-10 font-bold'>Low Inventory Summary</h1>
+			<table className=' text-black w-full min-w-full divide-y divide-gray-200'>
+				<thead className='text-black w-full max-w-full border-2 border-black rounded-3xl bg-gray-50'>
+					<tr className='mb-7'>
+						<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'></th>
+						<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Name</th>
+						<th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Quantity</th>
+					</tr>
+				</thead>
+				<tbody className='bg-white divide-y divide-gray-200 mt-4 overflow-scroll max-h-max'>
+					{lowInventoryList.map((item, index) => {
+						return (
+							<tr className='hover' key={uuidv4()}>
+								<th className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{index + 1}
+								</th>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{item.name}
+								</td>
+								<td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900' key={uuidv4()}>
+									{item.quantity}
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
 	);
 }
