e6476c5c2353e9f0b145921e4eea72d2b072a732
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2024-04-22 19:10:54 -0500

    UI merged master (#47)
    * new changes
    
    * bugfix - appointment page
    
    ---------
    
    Co-authored-by: Raja Krishna <rajakrishna214@gmail.com>

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,25 +1,25 @@
-'use client'
+"use client";
 
 import "./globals.css";
 import Link from "next/link";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/config";
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useEffect, useState } from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import useDrift from "./_hooks/liveChat-api/useDrift.js";
 
 export default function RootLayout({ children }) {
+  const [user, loading, error] = useAuthState(auth);
 
   useDrift();
-  const [ shouldUseDrift, setShouldUseDrift ] = useState(true);
+  const [shouldUseDrift, setShouldUseDrift] = useState(true);
   const pathname = usePathname();
-
 
   useEffect(() => {
     if (typeof window === "undefined") return;
 
-    let adminURL = pathname.includes('admin');
+    let adminURL = pathname.includes("admin");
 
     console.log(pathname, shouldUseDrift);
     if (adminURL && window.drift?.api?.widget) {
@@ -30,67 +30,52 @@ export default function RootLayout({ children }) {
     setShouldUseDrift(adminURL ? false : true);
   }, [pathname]);
 
-
-  const [user, loading, error] = useAuthState(auth)
-
-  if (loading) {
-    return (
-    <html
-      lang="en"
-      className=" bg-blue-600"
-      data-theme="aqua">
-       <body className="">
-       <header>
-       </header>
-       <div className="flex items-center justify-center min-h-screen bg-blue-600">
-        <span className="loading loading-bars loading-lg"></span>
-       </div>
-      <main className=" bg-blue-600">{children}</main>
-      </body>
-    </html>
-  )
-}
-
   return (
     <html
       lang="en"
-      className=" bg-blue-900"
-      data-theme="aqua">
-      <body className="">
-        <header className="flex justify-between w-full p-3 bg-neutral">
-          <Link href={"/"}>
-            <div className="btn bg-transparent border-0">logo here</div>
-          </Link>
-          <nav className="flex gap-3 items-center">
-            <Link href={"/appointment"}>
-              <div className="btn btn-sm bg-transparent border-0">Book Now</div>
-            </Link>
-
-            <Link href={"/companyservices"}>
-              <div className="btn btn-sm bg-transparent border-0">Services</div>
-            </Link>
-            { !user && (
-              <>
-            <Link href={"/signup"}>
-              <div className="btn btn-sm bg-transparent  border-2">Sign Up</div>
-            </Link>
-
+      data-theme="light">
+      <body>
+        <div className="max-w-screen-2xl mx-auto px-4 py-8">
+          <header className="flex bg-gray-100 border rounded-xl p-6 items-center">
             <Link
-              className="btn btn-sm bg-green-800"
-              href={"/login"}>
-              <div>Login</div>
+              href={"/"}
+              className="font-bold text-xl flex-1">
+              <div>Broken Pipe Plumbing</div>
             </Link>
-            </>
-          )}
-           { user &&  <Link
-              className="btn btn-sm btn-warning"
-              href={"/signout"}>
-              <div>Sign out</div>
-            </Link>
-          }
-          </nav>
-        </header>
-        <main className=" bg-blue-600">{children}</main>
+            <nav className="flex gap-4 items-center">
+              <Link href={"/appointment"}>
+                <div className="font-bold">Book Now</div>
+              </Link>
+
+              <Link href={"/companyservices"}>
+                <div className="font-bold">Services</div>
+              </Link>
+              {!user && (
+                <>
+                  <Link href={"/signup"}>
+                    <div className="border font-bold border-slate-900 p-2 rounded-lg w-24 text-center">
+                      Sign Up
+                    </div>
+                  </Link>
+
+                  <Link href={"/login"}>
+                    <div className="bg-slate-900 font-bold p-2 rounded-lg text-white w-24 text-center">
+                      Login
+                    </div>
+                  </Link>
+                </>
+              )}
+              {user && (
+                <Link
+                  className="btn btn-sm btn-warning"
+                  href={"/signout"}>
+                  <div>Sign out</div>
+                </Link>
+              )}
+            </nav>
+          </header>
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
